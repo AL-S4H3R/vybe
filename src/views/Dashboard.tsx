4e14784e94b8f9c98a94f09dd5d4b4c83ff1504a
Shlok D. Mange <shlok_mange@protonmail.com>
2021-03-30 02:38:49 +0400

    Added Dashboard MSG

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -7,8 +7,22 @@ const Dashboard: React.FC = () => {
     return(
         <div>
             <nav className="px-8 py-4">
-                <h1 className="text-2xl text-center font-mono text-gray-700">welcome to vybe</h1>
+                <h1 className="text-xl underline text-center font-mono text-gray-700">welcome to vybe</h1>
             </nav>
+            <section className="font-mono px-8 py-4 space-y-8 text-gray-700">
+                <div>
+                    <p className="text-center text-2xl">With Vybe, you can mint your digital NFT's (all for $0.)</p>
+                </div>
+                <div>
+                    <p className="text-center text-2xl">If you use Figma, you can directly drop in the figma URL and we'll create a token for you.</p>
+                </div>
+                <p className="text-center">
+                    Thank you for vybing!!
+                </p>
+                <p className="text-center text-lg font-semibold">
+                    App Goes Live Soon
+                </p>
+            </section>
         </div>
     )
 }

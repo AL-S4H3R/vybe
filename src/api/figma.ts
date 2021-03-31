8ed1fc3ce9b508d8dc6825df82b0cc0766009021
Shlok D. Mange <shlok_mange@protonmail.com>
2021-03-31 17:41:44 +0400

    Figma url -> File id

---
+++ src/api/figma.ts
@@ -0,0 +1,13 @@
+import axios from 'axios'
+const baseUrl = 'https://api.figma.com'
+
+export const getImageUrl = async (figmaUrl: string) => {
+
+    var figmaId = figmaUrl.split('/')[4]
+    var ids = '0:1'
+    let imageResult = await axios(`${baseUrl}/v1/images/${figmaId}?scale=2&ids=${ids}`, {
+        method: 'GET',
+        headers: {'X-Figma-Token': process.env.REACT_APP_FIGMA_ACCESS_TOKEN} 
+    })
+    return imageResult.data.images
+}
--- src/components/addFigma/Add.tsx
+++ src/components/addFigma/Add.tsx
@@ -1,6 +1,16 @@
 import React from 'react'
+import { getImageUrl } from '../../api/figma'
 
 const Add: React.FC = () => {
+
+    const [url, setUrl] = React.useState('')
+    
+    const getImg = async () => {
+        var ids = '0:1'
+        var imageRes = await getImageUrl(url)
+        console.log(imageRes[ids])
+    }
+
     return(
         <section className="px-8 py-4 lg:px-16 space-y-4">
             <div className="flex justify-center">
@@ -8,10 +18,17 @@ const Add: React.FC = () => {
                     type="text" 
                     className="bg-gray-200 w-full px-2 py-1 rounded-md lg:w-1/2 text-center"
                     placeholder="https://www.figma.com/file/7cXwmEAZhV2fJWctEreWTZ/Untitled"
+                    value={url}
+                    onChange={(e) => setUrl(e.target.value)}
                 />
             </div>
             <div className="flex justify-center">
-                <button className="px-2 py-1 rounded bg-gray-900 text-gray-200">Check Preview</button>
+                <button 
+                    className="px-2 py-1 rounded bg-gray-900 text-gray-200"
+                    onClick={() => getImg()}
+                >
+                    Check Preview
+                </button>
             </div>
         </section>
     )
